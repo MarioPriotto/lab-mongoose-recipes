80470b73512c3992354fcc56d768475b86fd4dc2
Mario Cezar Priotto <mariopriotto@gmail.com>
2022-11-30 21:46:17 -0300

    LabCumprido

--- index.js
+++ index.js
@@ -2,22 +2,111 @@ const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
+
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
-
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
+let conexao = null
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
+
+// Function para Conectar e Limpar BD
+const conectarLimpar = async () => {
+  await mongoose.connect(MONGODB_URI)
+  .then( x => {
+    //console.log(`Ponto de Conexão: "${x.connection.name}"`)
+    conexao = x.connection
+    //console.log(`Ponto de Conexão: "${conexao}"`)
+    console.log(`Ponto de Conexão: "${conexao.name}"`)
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then( x => {
+    console.log(`Ponto de Saída: "${conexao.name}"`)
+    //console.log(`Ponto de Saída: "${conexao}"`)
+    //return conexao
   });
+}
+
+//gravarDadosUnicoRegistro
+const gravarDadosUnicoRegistro = async () => {
+  const mDados = { 
+      title: "Bolo de chocolate", 
+      level: "Easy Peasy", 
+      ingredients: ["Farinha","Chocolate em pó","Acúcar"], 
+      cuisine: "Paranaense", 
+      dishType: "breakfast", 
+      //image: "", 
+      duration: 54, 
+      creator: "Aventureiro de fds" ,
+      //created: Date,
+  }
+  const executa = async () => {
+      const afetado = await Recipe.create(mDados);
+      if ( afetado ) {
+        console.log("01-Registro novo e ÚNICO incluído com sucesso...")
+        return afetado
+      } else {
+        console.log("01-*** ERRO em incluir registro novo e ÚNICO")
+        return "ERRO-01"
+      }
+  }
+  return await executa()
+}
+
+//gravarDadosDeTodaMatriz
+const gravarDadosMatriz = async () => {
+  const afetado = await Recipe.insertMany(data);
+  if ( afetado ) {
+    console.log("02-Gravação de dados da Matriz com sucesso...")
+    return afetado
+  } else {
+    console.log("02-*** ERRO - em gravar Dados MATRIZ")
+    return "ERRO-02"
+  }
+}
+
+//alterarDadosDeUmRegistro
+const alterarRegistro = async () => {
+  const query = { title: 'Rigatoni alla Genovese' };
+  const afetado = await Recipe.findOneAndUpdate(query, { duration: 100 }, { new: true, runValidators: true} )
+  if ( afetado ) {
+    console.log("03-Registro alterado com sucesso...")
+    return afetado
+  } else {
+    console.log("03-*** ERRO - em registro alterado")
+    return "ERRO-03"
+  }
+}
+
+//deletarDadosDeUmRegistro
+const deletarRegistro = async () => {
+  const query = { title: 'Carrot Cake' };
+  const afetado = await Recipe.findOneAndRemove(query)
+  if ( afetado ) {
+    console.log("04-Registro deletado com sucesso...")
+    return afetado
+  } else {
+    console.log("04-*** ERRO - em registro deletado")
+    return "ERRO-04"
+  }
+}
+
+//fechar o arquivo -> somente após todos os processos
+const fecharArquivo = async () => {
+  conexao.close()
+  console.log("05-Arquivo fechado")
+}
+
+//gerenciarDisparosDasChamadas
+const gerenciarChamadas = async () => {
+  console.log("\nPONTO DE ENTRADA ======================================")
+  await conectarLimpar()
+  await gravarDadosUnicoRegistro()
+  await gravarDadosMatriz()
+  await alterarRegistro()
+  await deletarRegistro()
+  await fecharArquivo(conexao)
+  console.log("PONTO DE SAIDA ========================================\n")
+}
+gerenciarChamadas()
+
